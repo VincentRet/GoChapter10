3b6684cbd6543d5d410421adfa7f93e03d1102ac
VincentRet <115279323+VincentRet@users.noreply.github.com>
2025-04-04 09:48:02 -0400

    Initial commit with Add function

---
+++ add.go
@@ -0,0 +1,7 @@
+// add.go
+package addmodule
+
+// Add adds two integers and returns the result.
+func Add(a int, b int) int {
+	return a + b
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/VincentRet/GoChapter10
+
+go 1.23.5
