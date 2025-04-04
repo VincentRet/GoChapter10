1debd1a51ae6e50a560342d9e9f16d9c813a0de7
VincentRet <115279323+VincentRet@users.noreply.github.com>
2025-04-04 09:51:26 -0400

    Add godoc comments for package and Add function (v1.0.1)

--- add.go
+++ add.go
@@ -1,7 +1,10 @@
-// add.go
+// Package addmodule provides utility functions for basic arithmetic operations.
+// Currently, it includes a simple addition function for integers.
 package addmodule
 
-// Add adds two integers and returns the result.
+// Add returns the sum of two integers.
+// For more about how addition works, see:
+// https://www.mathsisfun.com/numbers/addition.html
 func Add(a int, b int) int {
 	return a + b
 }
